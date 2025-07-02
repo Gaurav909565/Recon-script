210b53419d6a299f74e0d5ffc2b5e20b80d6a0dd
Gaurav verma <146462299+Gaurav909565@users.noreply.github.com>
2025-07-02 16:55:35 +0530

    Update recon.sh
    added vulnearbility scan option

--- recon.sh
+++ recon.sh
@@ -2,137 +2,97 @@
 
 source ./scan.lib
 
-while getopts "m:ih" OPTION; do
+# Default behavior
+VULN_SCAN=false
+INTERACTIVE=false
+
+# Help menu
+show_help() {
+    echo "Usage: $0 [options] [domain]"
+    echo
+    echo "Options:"
+    echo "  -i          Interactive mode. Enter domains one by one."
+    echo "  -v          Run vulnerability scans (Nmap, Nuclei)."
+    echo "  -h          Show this help message."
+    echo
+    echo "Examples:"
+    echo "  $0 example.com"
+    echo "  $0 -v example.com"
+    echo "  $0 -i"
+}
+
+# Parse options
+while getopts "ivh" OPTION; do
   case $OPTION in
-    m)
-      MODE=$OPTARG
-      ;;
     i)
       INTERACTIVE=true
       ;;
+    v)
+      VULN_SCAN=true
+      ;;
     h)
-      # Display all available modes
-      echo "Available modes with -m :"
-      echo "-m crt        : Perform a crt.sh scan"
-      echo "-m subs       : Perform a subdomain scan"
-      echo "-m alive_subs : Perform an alive subdomain scan"
-      echo "-m dir        : Perform a directory scan"
-      echo "-m xss        : Perform an XSS scan"
+      show_help
       exit 0
       ;;
     *)
-      echo "Invalid option"
+      show_help
       exit 1
       ;;
   esac
 done
 
-scan_domain(){
-  domain=$1
-  if [[ "$domain" =~ ^https?:// ]]; then
-    # Remove https:// and http://
-    domain=${domain#https://}
-    domain=${domain#http://}
-    echo "[-] Removing https:// or http:// from $domain"
-  fi
+shift $((OPTIND - 1))
 
-  # Check if it does not end with a /
-  if [[ "$domain" != */ ]]; then
-    echo "Domain : $domain"
-  else
-    # Remove trailing /
-    domain=${domain%/}
-    echo "[-] Removing / from $domain"
-  fi
-  directory=${domain}_recon
-  echo "[+] Creating directory $directory and $directory/Extra"
-  rm -rf $directory
-  mkdir $directory
-  mkdir $directory/Extra
-  case $MODE in
-    crt)
-      crt_scan
-      ;;
-    subs)
-      subdomain_scan
-      ;;
-    alive_subs)
-      alive_subdomain_scan
-      ;;
-    dir)
-      directory_scan
-      ;;
-    xss)
-      xss_scan
-      ;;
-    *)
-      crt_scan
-      subdomain_scan
-      alive_subdomain_scan
-      directory_scan
-      ;;
-  esac
-}
+# --- Main Functions ---
 
-report_domain(){
-  domain=$1
-  if [[ "$domain" =~ ^https?:// ]]; then
-    # Remove https:// and http://
-    domain=${domain#https://}
-    domain=${domain#http://}
-    echo "[-] Removing https:// or http:// from $domain"
-  fi
-  # Check if it does not end with a /
-  if [[ "$domain" != */ ]]; then
-    echo "Domain : $domain"
-  else
-    # Remove trailing /
-    domain=${domain%/}
-    echo "[-] Removing / from $domain"
-  fi
-  directory=${domain}_recon
-  echo "[+] Generating recon report for $domain..."
-  today=$(date)
-  echo "[+] This scan was created on $today" > $directory/report
-  if [ -f $directory/crt ];then
-    echo "[+] Results for crt.sh:" >> $directory/report
-    jq -r ".[] | .name_value" $directory/crt >> $directory/report
-  fi
-  if [ -f $directory/subdomain.txt ];then
-    echo "[+] Results for subdomain scan:" >> $directory/report
-    cat $directory/subdomain.txt >> $directory/report
-  fi
-  if [ -f $directory/alive_subdomains.txt ];then
-    echo "[+] Results for alive subdomain scan:" >> $directory/report
-    cat $directory/alive_subdomains.txt >> $directory/report
-  fi
-  if [ -f $directory/allurls.txt ];then
-    echo "[+] Results for directory scan:" >> $directory/report
-    cat $directory/allurls.txt >> $directory/report
-  fi
-  if [ -f $directory/param.txt ];then
-    echo "[+] Results for parameters scan:" >> $directory/report
-    cat $directory/param.txt >> $directory/report
-  fi
-  if [ -f $directory/xss_report.txt ];then
-    echo "[+] Results for XSS scan:" >> $directory/report
-    cat $directory/xss_report.txt >> $directory/report
-  fi
-}
-
-if [ $INTERACTIVE ];then
-  input="BLANK"
-  while [ $input != "exit" ];do
-    echo "Please enter a domain > "
-    read input
-    if [ $input != "exit" ];then
-      scan_domain $input
-      report_domain $input
+run_recon() {
+    domain=$1
+    if [[ -z "$domain" ]]; then
+        error "No domain provided."
+        return
     fi
-  done
+
+    # Sanitize domain input
+    domain=$(echo "$domain" | sed -e 's|^https?://||' -e 's|/$||')
+    info "Starting reconnaissance for: $domain"
+    
+    directory="${domain}_recon"
+    info "Creating output directory: $directory"
+    rm -rf "$directory"
+    mkdir -p "$directory/Extra"
+
+    # Execute reconnaissance workflow
+    gather_subdomains
+    check_live_subdomains
+    screenshot_subdomains
+    categorize_subdomains
+    gather_urls
+    categorize_urls_by_extension
+    find_secrets
+    categorize_urls_by_impact
+
+    # Run vulnerability scan if -v flag is provided
+    if [ "$VULN_SCAN" = true ]; then
+        run_vulnerability_scan
+    fi
+
+    info "Reconnaissance complete for $domain."
+}
+
+# --- Execution Logic ---
+
+if [ "$INTERACTIVE" = true ]; then
+    while true; do
+        read -p "Enter a domain (or 'exit' to quit): " input
+        if [[ "$input" == "exit" ]]; then
+            break
+        fi
+        run_recon "$input"
+    done
 else
-  for i in "${@:$OPTIND:$#}";do
-    scan_domain $i
-    report_domain $i
-  done
+    if [ -z "$1" ]; then
+        error "No domain provided. Use -h for help."
+        exit 1
+    fi
+    run_recon "$1"
 fi
